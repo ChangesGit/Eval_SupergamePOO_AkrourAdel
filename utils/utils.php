a970dfef8c9e589ec23fffe9bee295907526d3ec
ChangesGit <akrour.adel.pro@gmail.com>
2026-09-04 12:21:20 +0200

    FIX : Env variables

--- utils/utils.php
+++ utils/utils.php
@@ -4,19 +4,10 @@ function sanitize(string $data):string {
     return trim($data);
 }
 
-// function connect():PDO {
-//     return new PDO('mysql:host='.$_ENV['dbhost'].';
-//     dbname='.$_ENV['dbhost'],
-//     $_ENV['login'],
-//     $_ENV['password'],
-//     [
-//         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-//     ]);
-// }
 
 function connect():PDO {
-    return new PDO('mysql:host=localhost;
-    dbname=supergame',
+    return new PDO("mysql:host={$_ENV['dbhost']};
+    dbname={$_ENV['dbname']}",
     $_ENV['login'],
     $_ENV['password'],
     [
